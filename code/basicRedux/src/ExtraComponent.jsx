1bd7ba864e24c728e391694ae3e7286dd9de634d
Yash-6503 <yashwalke6503@gmail.com>
2025-04-13 23:52:32 +0530

    update

---
+++ code/basicRedux/src/ExtraComponent.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { useSelector } from 'react-redux'
+
+function ExtraComponent() { 
+    const c = useSelector((state) => state.count)
+    return (
+        <div>
+            {c}
+        </div>
+    )
+}
+
+export default ExtraComponent
